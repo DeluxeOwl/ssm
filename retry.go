eb6be8090d875da8d19dc5d91060327db04420e5
Marius Orcsik <marius@federated.id>
2024-02-04 09:39:15 +0100

    Clarify some documentation about Retry

--- retry.go
+++ retry.go
@@ -6,22 +6,13 @@ import (
 	"time"
 )
 
-// Retry is a way to construct a state machine out of repeating the execution of received state "fn", until the number of
-// "retries" has been reached, or "fn" returns the End state.
-// The "strategy" parameter can be used as a way to delay the execution between retries. If nil is passed the Batch
-// aggregator function is used, which just executes the code sequentially without pause.
+// Retry is a way to construct a state machine out of repeating the execution of received state "fn",
+// until the number of "retries" has been reached, or "fn" returns the End state.
+//
+// The "fn" parameter can be one of the functions accepting a StrategyFn parameters,
+// which wrap the original state Fn, and which provide a way to delay the execution between retries.
 func Retry(retries int, fn Fn) Fn {
 	return retry(retries, fn)
-}
-
-// BackOff returns an aggregator function which can be used to execute the received state with increasing delays.
-// The function for determining the delay is passed in the StrategyFn "dur" parameter.
-//
-// There is no end condition, so take care to limit the execution through some external method.
-func BackOff(dur StrategyFn, fn Fn) Fn {
-	return func(ctx context.Context) Fn {
-		return after(dur()).run(fn)(ctx)
-	}
 }
 
 func retry(retries int, fn Fn) Fn {
@@ -41,6 +32,16 @@ func retry(retries int, fn Fn) Fn {
 
 // StrategyFn is the type that returns the desired time.Duration for the BackOff function.
 type StrategyFn func() time.Duration
+
+// BackOff returns an aggregator function which can be used to execute the received state with increasing delays.
+// The function for determining the delay is passed in the StrategyFn "dur" parameter.
+//
+// There is no end condition, so take care to limit the execution through some external method.
+func BackOff(dur StrategyFn, fn Fn) Fn {
+	return func(ctx context.Context) Fn {
+		return after(dur()).run(fn)(ctx)
+	}
+}
 
 // Constant returns a constant time.Duration for every call.
 func Constant(d time.Duration) StrategyFn {
